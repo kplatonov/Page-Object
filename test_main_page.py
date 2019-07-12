90f53a90d24274c3bfbd790d04e9f154b8626be1
Platonov Konstantin <k.platonov@mail.msk>
2019-07-12 14:34:45 +0300

    4.2 Пишем первые тесты с помощью Page Object, exercise-1

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,13 @@
 """
 pytest -v --tb=line --language=en test_main_page.py
 """
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+def go_to_login_page(browser):
+    link = browser.find_element_by_css_selector("#login_link")
+    link.click()
+
+def test_guest_can_go_to_login_page(browser): 
+    browser.get(link) 
+    go_to_login_page(browser)
