e3d13aba4ddde14cc2c984848ea5b5fe99b408f2
Platonov Konstantin <k.platonov@mail.msk>
2019-07-15 18:22:02 +0300

    Переходы между страницами

--- pages/main_page.py
+++ pages/main_page.py
@@ -2,16 +2,20 @@
 pytest -v --tb=line --language=en test_main_page.py
 """
 
-from .base_page import BasePage
+from base_page import BasePage
 from selenium.webdriver.common.by import By
+from login_page import LoginPage
 
 from .locators import MainPageLocators
 
 class MainPage(BasePage):
 
     def go_to_login_page(self):
-        link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-        link.click() 
+        #link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
+        return LoginPage(browser=self.browser, url=self.browser.current_url) 
+
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"  #* для распаковки кортежа
---
+++ pages/test_page_object.py
@@ -0,0 +1,27 @@
+﻿from main_page import MainPage
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
+
+
+
+#second approach
+#comment last line in go_to_login_page:
+#def go_to_login_page(self):
+#    link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+#    link.click()
+#    # return LoginPage(browser=self.browser, url=self.browser.current_url) 
+
+from pages.login_page import LoginPage
+
+def test_guest_can_go_to_login_page_2(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
