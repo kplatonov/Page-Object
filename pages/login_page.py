baf2a58fb6799b62624dc4384f366f712a283915
Platonov Konstantin <k.platonov@mail.msk>
2019-07-15 18:13:50 +0300

    Реализация LoginPage

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,5 @@
-from .base_page import BasePage
-
+from base_page import BasePage
+from locators import LoginPageLocators
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -9,12 +9,16 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
+        #assert self.browser.current_url == self.url, "Login link invalid"
+        assert 'login' in self.browser.current_url
         assert True
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(By.CSS_SELECTOR, LoginPageLocators.LOGIN_USERNAME), "There isn't login form"
         assert True
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(By.CSS_SELECTOR, LoginPageLocators.REGISTRATION_EMAIL), "There isn't register form"
         assert True
